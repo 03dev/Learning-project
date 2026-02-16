a8b8224ac8e36a45ebf501398fc13fee01999441
Dev <03devverma@gmail.com>
2026-02-16 13:55:00 +0530

    refactor: improve query validation and error handling in notes retrieval

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -2,11 +2,8 @@ import { Router, Request, Response } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { noteSchema, updateNoteSchema } from "../validators/note.schema";
 import prisma from "../db/prisma";
-import { error } from "node:console";
-// import { Prisma } from "../generated/prisma/browser";
 import { Prisma } from "@prisma/client";
 import { noteQuerySchema } from "../validators/noteQuerySchema";
-
 
 const router = Router();
 
@@ -44,36 +41,36 @@ router.get("/notes", authMiddleware, async (req: Request, res: Response) => {
 
   const parsedQuery = noteQuerySchema.safeParse(req.query);
 
-  if(!parsedQuery.success) {
+  if (!parsedQuery.success) {
     return res.status(400).json({
       message: "Invalid query parameters",
-      error: parsedQuery.error.format()
+      error: parsedQuery.error.format(),
     });
   }
 
-  let { page, limit, search, sortBy, sortOrder} = parsedQuery.data;
-      
+  let { page, limit, search, sortBy, sortOrder } = parsedQuery.data;
+
   let orderBy: Prisma.NoteOrderByWithRelationInput = {
     [sortBy]: sortOrder,
   };
 
   let whereCondition: Prisma.NoteWhereInput = {
-  userId
-};
+    userId,
+  };
 
   if (search) {
     whereCondition.OR = [
-        {
-            title: {
-                contains: search,
-            }
+      {
+        title: {
+          contains: search,
         },
-        {
-            content: {
-                contains: search,
-            }
-        }
-    ]
+      },
+      {
+        content: {
+          contains: search,
+        },
+      },
+    ];
   }
 
   if (isNaN(page)) page = 1;
@@ -91,14 +88,14 @@ router.get("/notes", authMiddleware, async (req: Request, res: Response) => {
 
   const [totalNotes, notes] = await Promise.all([
     prisma.note.count({
-        where: whereCondition
+      where: whereCondition,
     }),
     prisma.note.findMany({
-        where: whereCondition,
-        skip,
-        take: limit,
-        orderBy,
-    })
+      where: whereCondition,
+      skip,
+      take: limit,
+      orderBy,
+    }),
   ]);
 
   const totalPages = Math.ceil(totalNotes / limit);
@@ -128,23 +125,38 @@ router.delete(
       });
     }
 
-    const note = await prisma.note.findUnique({
+    // Check if note exists and belongs to user
+
+    // const note = await prisma.note.findUnique({
+    //   where: {
+    //     id: note_id,
+    //   },
+    // });
+
+    // if (!note || note.userId !== userId) {
+    //   return res.status(403).json({
+    //     message: "Note not found or unauthorized",
+    //   });
+    // }
+
+    // await prisma.note.delete({
+    //   where: {
+    //     id: note_id,
+    //   },
+    // });
+
+    const result = await prisma.note.deleteMany({
       where: {
         id: note_id,
+        userId,
       },
     });
 
-    if (!note || note.userId !== userId) {
+    if (result.count === 0) {
       return res.status(403).json({
         message: "Note not found or unauthorized",
       });
     }
-
-    await prisma.note.delete({
-      where: {
-        id: note_id,
-      },
-    });
 
     return res.status(200).json({
       message: "Note deleted successfully",
@@ -197,6 +209,22 @@ router.put(
         ...result.data,
       },
     });
+
+    // const updated_note = await prisma.note.updateMany({
+    //   where: {
+    //     id: note_id,
+    //     userId: userId,
+    //   },
+    //   data: {
+    //     ...result.data,
+    //   }
+    // });
+
+    // if (updated_note.count === 0) {
+    //   return res.status(403).json({
+    //     message: "Note not found or unauthorized",
+    //   });
+    // }
 
     return res.status(200).json({
       message: "Note updated successfully",
